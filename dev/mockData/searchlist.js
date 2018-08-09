73a53e9a99aa545c964552eed570308406f6b7b1
Qiao <chun.qiao@accenture.com>
2018-08-09 17:34:26 +0800

    searchlist

---
+++ dev/mockData/searchlist.js
@@ -0,0 +1,342 @@
+export default [
+    {
+      word: '阿蛋',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '阿史',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: '阿蛋',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '阿史',
+      src: require('@/assets/images/tx2.jpg')
+    },
+    {
+      word: 'bbb',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '白白白',
+      src: require('@/assets/images/tx2.jpg')
+    },
+    {
+      word: 'ccc',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '菜菜菜',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: 'ccc',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '菜菜菜',
+      src: require('@/assets/images/tx2.jpg')
+    },
+    {
+      word: 'ddd',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'dadadad',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: 'dqdqdq',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'DDD',
+      src: require('@/assets/images/tx2.jpg')
+    },
+    {
+      word: 'eeee',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'e',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: 'eqqq',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'EEEE',
+      src: require('@/assets/images/tx2.jpg')
+    },
+    {
+      word: 'fafafa',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'FFFF',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: '发发发',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'GGGG',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '哥哥哥哥',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: '歌王',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '哈哈哈',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'imac',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'iphone',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: 'ipad',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'jack',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'Jasy',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: 'jony',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '卡车',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '卡住',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: '靠',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '卡车',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '卡住',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: '靠',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '来呀',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '来嘛',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: '来不来',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'Moon',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'Monther',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: 'Monday',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'New',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'Nuthon',
+      src: require('@/assets/images/tx2.jpg')
+		},
+    {
+      word: 'oooo',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '哦哦哦哦哦哦',
+      src: require('@/assets/images/tx2.jpg')
+		},
+    {
+      word: '屁',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '皮了个皮',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: '皮皮C',
+      src: require('@/assets/images/tx1.jpg')
+    },
+		{
+      word: 'PPC',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '去你大爷',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '去吧皮卡丘',
+      src: require('@/assets/images/tx2.jpg')
+		},
+    {
+      word: '日',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '圣光围绕的QCC',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'sara',
+      src: require('@/assets/images/tx2.jpg')
+		},
+    {
+      word: '弹弹弹',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'UUUU',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'vava',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'victory',
+      src: require('@/assets/images/tx2.jpg')
+    },
+    {
+      word: 'VC泡腾片代购',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'vuvu',
+      src: require('@/assets/images/tx2.jpg')
+    },
+    {
+      word: 'vue',
+      src: require('@/assets/images/tx2.jpg')
+		},
+    {
+      word: '我',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '我去',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '我靠',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'xixi',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '翔',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '悠悠',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'YY',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: 'Y哥',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '走开',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '走你',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '走起来',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: ',.[]',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '-=09',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '!@$#^',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '%%%cc',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '&&%%',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: '+_+__',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '>.<',
+      src: require('@/assets/images/tx2.jpg')
+    },
+    {
+      word: '":":"',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '{}{}{}}',
+      src: require('@/assets/images/tx2.jpg')
+		},
+		{
+      word: '!@#$%^',
+      src: require('@/assets/images/tx1.jpg')
+    },
+    {
+      word: '+_)(*&',
+      src: require('@/assets/images/tx2.jpg')
+    }
+  ]
